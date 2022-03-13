4f762d7d8468453779915c192d2c1c46131ea25f
Uğur Yürümez <73554899+uqursuz0@users.noreply.github.com>
2022-03-13 21:57:22 +0300

    Randomize 1.1
    The feature of using program again and again is added.

--- randomize.py
+++ randomize.py
@@ -1,7 +1,13 @@
 import random
-görevsayısı=int(input("seçenek sayısını giriniz"))
-görevlistesi=[]
-for i in range(görevsayısı):
-    elem = input(f"{i+1}.görevi yazınız.")
-    görevlistesi.append(elem)
-print(random.choice(görevlistesi))
+def görev():
+    try:
+        görevsayısı=int(input("seçenek sayısını giriniz"))
+        görevlistesi=[]
+        for i in range(görevsayısı):
+            elem = input(f"{i+1}.görevi yazınız.")
+            görevlistesi.append(elem)
+        print(random.choice(görevlistesi))
+        görev()
+    except:
+        print("done")
+görev()
